c02a93eeb8b9169c5b1c8c64563a75af66686d4e
loren <lorenmichaelh@gmail.com>
2022-07-12 14:10:53 -0500

    completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown (time)
+    while time >= 1
+        puts "#{time} SECOND(S)!"
+        # sleep 1.seconds
+        time -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (time)
+    while time >= 1
+        puts "#{time} SECOND(S)!"
+        sleep(1)
+        time -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
